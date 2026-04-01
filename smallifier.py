d041c231e175d28c0cc1d9a099621240a9c6d01f
bdblock94 <bdblock94@gmail.com>
2026-03-31 22:40:47 -0400

    stated adding functions

--- smallifier.py
+++ smallifier.py
@@ -1,23 +1,61 @@
 import pyperclip
+import sys
+import random
 
 
-to_smallify = pyperclip.paste()
-to_smallify = to_smallify.lower()
+text = pyperclip.paste()
+text = text.lower()
+
 
 
 bigs   = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","0"]
 smalls = ["ᵃ","ᵇ","ᶜ","ᵈ","ᵉ","ᶠ","ᵍ","ʰ","ᶦ","ʲ","ᵏ","ˡ","ᵐ","ⁿ","ᵒ","ᵖ","ᵠ","ʳ","ˢ","ᵗ","ᵘ","ᵛ","ʷ","ˣ","ʸ","ᶻ","¹","²","³","⁴","⁵","⁶","⁷","⁸","⁹","⁰"]
-smallified = ""
 
 
-for letter in range(0, len(to_smallify)):
-    if to_smallify[letter].isalnum():
-        if to_smallify[letter] in smalls:
-            smallified += to_smallify[letter]
+
+def random_case():
+    smallified = ""
+    for letter in range(0, len(text)):
+        if random.randint(0, 10) % 2 == 0:
+            if text[letter].isalnum():
+                if text[letter] in smalls:
+                    smallified += text[letter]
+                else:
+                    smalls_index = bigs.index(text[letter])
+                    smallified += smalls[smalls_index]
+            else:
+                smallified += text[letter]
         else:
-            smalls_index = bigs.index(to_smallify[letter])
-            smallified += smalls[smalls_index]
+            smallified += text[letter]
+    print(smallified)
+    pyperclip.copy(smallified)
+
+
+
+def smallify():
+    smallified = ""
+    for letter in range(0, len(text)):
+        if text[letter].isalnum():
+            if text[letter] in smalls:
+                smallified += text[letter]
+            else:
+                smalls_index = bigs.index(text[letter])
+                smallified += smalls[smalls_index]
+        else:
+            smallified += text[letter]
+    pyperclip.copy(smallified)
+    print(smallified)
+
+
+
+def main():
+    if len(sys.argv) != 2:
+        smallify()
     else:
-        smallified += to_smallify[letter]
-pyperclip.copy(smallified)
-print(smallified)
+        if sys.argv[1] == "-r":
+            random_case()
+    
+
+
+
+main()
