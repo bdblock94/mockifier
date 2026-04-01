fc16ad252a664e3f62e2f976e654b5811737a7ee
bdblock94 <bdblock94@gmail.com>
2026-04-01 10:10:30 -0400

    major refactor and added the ability to randomize size with -r or randomize upper/lower case with -c argument

--- smallifier.py
+++ smallifier.py
@@ -4,68 +4,66 @@ import random
 
 
 text = pyperclip.paste()
-text = text.lower()
-
+output = ""
 
 
 bigs   = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","0"]
 smalls = ["ᵃ","ᵇ","ᶜ","ᵈ","ᵉ","ᶠ","ᵍ","ʰ","ᶦ","ʲ","ᵏ","ˡ","ᵐ","ⁿ","ᵒ","ᵖ","ᵠ","ʳ","ˢ","ᵗ","ᵘ","ᵛ","ʷ","ˣ","ʸ","ᶻ","¹","²","³","⁴","⁵","⁶","⁷","⁸","⁹","⁰"]
 
 
+def make_small(ltr):
+    ltr = ltr.lower()
+    if ltr.isalnum() and ltr not in smalls:
+        return smalls[bigs.index(ltr)]
+    else:
+        return ltr
 
 
-def random_case():
-    print("Try again when -c is updated")
+
+def random_case(ltr):
+    if random.randint(1,2) == 2:
+        return ltr.upper()
+    else:
+        return ltr.lower()
 
 
 
-def random_size():
-    randomized = ""
-    for letter in range(0, len(text)):
-        if random.randint(0, 10) % 2 == 0:
-            if text[letter].isalnum():
-                if text[letter] in smalls:
-                    randomized += text[letter]
-                else:
-                    smalls_index = bigs.index(text[letter])
-                    randomized += smalls[smalls_index]
-            else:
-                randomized += text[letter]
+def random_size(ltr):
+    if random.randint(1,2) == 2:
+        return make_small(ltr)
+    else:
+        return ltr
+
+
+
+def output_one():
+    output = ""
+    for ltr in text:
+        output += make_small(ltr)
+    pyperclip.copy(output)
+    print(output)
+
+
+
+def output_two():
+    output = ""
+    for ltr in text:
+        if sys.argv[1] == "-r":
+            output += random_size(ltr)
+        elif sys.argv[1] == "-c":
+            output += random_case(ltr)
         else:
-            randomized += text[letter]
-    print(randomized)
-    pyperclip.copy(randomized)
-
-
-
-def smallify():
-    smallified = ""
-    for letter in range(0, len(text)):
-        if text[letter].isalnum():
-            if text[letter] in smalls:
-                smallified += text[letter]
-            else:
-                smalls_index = bigs.index(text[letter])
-                smallified += smalls[smalls_index]
-        else:
-            smallified += text[letter]
-    pyperclip.copy(smallified)
-    print(smallified)
-
+            sys.exit("Warning: Invalid argument, did you mean to use -r or -c?")
+    pyperclip.copy(output)
+    print(output)
+        
 
 
 def main():
-    if len(sys.argv) != 2:
-        smallify()
-        sys.exit(0)
-    elif sys.argv[1] == "-r":
-        random_size()
-        sys.exit(0)
-    elif sys.argv[1] == "-c":
-        random_case()
+    if len(sys.argv) >= 2:
+        output_two()
     else:
-        sys.exit("Warning: Invalid argument, did you mean to use -r or -c?")
-    
+        output_one()
 
 
 
